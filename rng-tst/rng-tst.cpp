bf8e5a9ec12b1381e567490ececf8cd9b88dbbc6
alexandr Savin <alexandr.d.savin@gmail.com>
2022-09-10 22:30:56 +0300

    Added memory cleanup.

--- rng-tst/rng-tst.cpp
+++ rng-tst/rng-tst.cpp
@@ -30,6 +30,8 @@ int main(int argc, char* argv[]) {
 
     int m = movethrough(arrpoint);
 
+    delete[]arrpoint;
+
     printf("Took %i moves to make it!\n", m);
 
 
@@ -101,7 +103,8 @@ int* toaddr(int32_t* arr, int size) {
         }
 
     }
-
+    delete[]tmparr;
+    /*
     for (i = 0; i < size; i++) {
 
         //printf("%i. %i\n" ,i, tmparr2[i]);
@@ -109,7 +112,9 @@ int* toaddr(int32_t* arr, int size) {
     }
 
     printf("\n");
+    */
     return tmparr2;
+    
 }
 
 
@@ -140,13 +145,3 @@ int* rng(int num) {
 }
 
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
